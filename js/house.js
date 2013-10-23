695ecaf47450bf6d84d3b34229f8c502eb3a0acf
thew00d <thew00d@hotmail.com>
2013-10-23 21:18:12 +0100

    Added the description mechanism

--- js/house.js
+++ js/house.js
@@ -10,6 +10,8 @@ $( document ).ready(function() {
 	$("#goleft").hide();
 	$("#goright").hide();
 	$("#goback").hide();
+	$("header h2").text("Front Door");
+	$("#description").text(positionDescription("Front Door"));
 
 	main();
 
@@ -118,8 +120,8 @@ $( document ).ready(function() {
 					$("#goright").show();
 				}
 
-				//$("#description").text(housePositions[currentPosition.verboseDescription]);
-				$("'description").text = "test";
+				$("#description").text(housePositions[currentPosition].verboseDescription);
+				$("header h2").text(housePositions[currentPosition].description);
 
 				return false; 
 			} 
@@ -135,7 +137,6 @@ $( document ).ready(function() {
 	  this.leftPosition = leftPosition;
 	  this.rightPosition = rightPosition;
 	  this.description = description;
-	  console.log(positionDescription(this.description));
 	  this.verboseDescription = positionDescription(this.description);
 	  addArea(description);
 	}
@@ -230,14 +231,54 @@ $( document ).ready(function() {
 		switch(position)
 		{
 			case 'Front Door':
-				posDes = "this is the front door";
+				posDes = "This is the front door.  As front doors go, it's fairly standard.  It has a lock which a key can be inserted into and with no more than a 40 degree turn to the left the door will open.";
 				return posDes;
 				break;
 
 			case 'Hallway':
-				posDes = "this is the hallway";
+				posDes = "You now find yourself in the hallway.  This provides several options to you.  You can go up the stairs to the 2nd floor, or to your right there is the living room.  Shoes should be removed here to avoid any stainage on the cream carpets that are fitted throughout the house.";
 				return posDes;
 				break;
+
+			case 'Stairs':
+				posDes = "You are climbing the stairs.  This means you are either heading up to the 2nd floor or descending once again.  If you were to ascend and descend the stairs for 20 times in a row, this would probably provide you with a fairly nice workout.";
+				return posDes;
+				break;				
+
+			case 'Living Room':
+				posDes = "The living room is an idea place for sitting in front of the TV with your dinner and a beer or possibly a nice glass of Chablis.  You may prefer to call it the lounge but that is probably because you are working class.";
+				return posDes;
+				break;				
+
+			case 'Back Garden':
+				posDes = "The back garden is a great place to come if you want to get to the shed.  In the shed there are many treasures, mostly for tidying up the front and back gardens.  You might want to consider concreting over the front and back gardens and having the shed demolished.";
+				return posDes;
+				break;		
+
+			case 'Kitchen':
+				posDes = "The kitchen is an ideal place to be if you have something that needs cooking - be it a can of Heinz Macaroni Cheese or a packet of noodles.  Once you have cooked either of these, the dishwasher will be able to provide you with cleaning facilities.";
+				return posDes;
+				break;		
+
+			case 'Bedroom':
+				posDes = "If you are tired, the bedroom is the place to be.  ";
+				return posDes;
+				break;	
+
+			case 'Landing':
+				posDes = "this is the Landing";
+				return posDes;
+				break;		
+
+			case 'Spare Room':
+				posDes = "this is the spare room";
+				return posDes;
+				break;			
+
+			case 'Bathroom':
+				posDes = "this is the bathroom";
+				return posDes;
+				break;		
 
 			default:
 				posDes = "this is somewhere else";
